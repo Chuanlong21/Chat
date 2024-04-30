33c0aa61123e8006c4860c7cc47ef2acb9a042b4
Chuanlon21 <chuanlon@buffalo.edu>
2024-04-29 21:45:43 -0400

    set up

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Chuanlong Liu on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
